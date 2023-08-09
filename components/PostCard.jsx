efaa47882fe98df45508f8ad5b793029adb11653
MMar1ey13 <mwendamarley12@gmail.com>
2023-08-09 20:01:30 +0300

    deploy

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -14,7 +14,7 @@ const PostCard = ({ post }) => {
         className='object-center absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg'
         />
       </div>
-      <h1 className='transition duration-700 text-white text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold'>
+      <h1 className='transition duration-300 text-white text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold'>
         <Link href={`/post/${post.slug}`}>
           {post.title}
         </Link>
