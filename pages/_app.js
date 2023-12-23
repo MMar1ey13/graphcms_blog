e6902d2404fab8067beb199161d9968ab873313c
MMar1ey13 <mwendamarley12@gmail.com>
2023-12-23 04:32:01 +0300

    insights

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,11 @@ import React, { useEffect, useState} from 'react';
 import { Layout } from '@/components';
 import 'tailwindcss/tailwind.css'; 
 import '@/styles/globals.scss'
+import analytics from '@vercel/analytics';
+
+// Initialize Vercel Analytics with your project ID
+analytics.init({ projectId: 'prj_5TOgyhuqujwnhpcV7hTFJab1EXJR' });
+
 
 function MyApp({ Component, pageProps }) {
   return (
