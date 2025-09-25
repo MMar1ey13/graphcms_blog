3c7f755d3db7a4195e7e00c6571a0ee94c6dd16a
MARLEY <127131158+MMar1ey13@users.noreply.github.com>
2025-09-26 01:49:58 +0300

    Update Categories.jsx

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -12,7 +12,7 @@ const Categories = () => {
     });
   }, []);
   return (
-    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+    <div className="bg-black shadow-lg rounded-lg p-8 pb-12 mb-8">
       <h3 className="text-gray-700 text-xl mb-8 font-semibold border-b pb-4">Categories</h3>
       {categories.map((category, index) => (
         <Link key={index} href={`/category/${category.slug}`}>
